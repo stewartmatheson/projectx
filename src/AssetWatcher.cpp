ea51549df98768067d1ba2c4191f2f02f0f8a320
Tom Sullivan <tom@msbit.com.au>
2021-09-19 15:13:51 +1000

    Move to using `WaitForSingleObject`
    As there is only one object being waited on, move from using
    `WaitForMultipleObjects` to `WaitForSingleObject`

--- src/AssetWatcher.cpp
+++ src/AssetWatcher.cpp
@@ -34,21 +34,19 @@ void AssetWatcher::Reload() {
 void AssetWatcher::StartWatching() {
     std::string asset_path =
         "C:\\Users\\Stewart\\SourceCode\\projectx\\build\\assets";
-    HANDLE directory_watcher_change_handles[1];
     LPTSTR lp_asset_path = new TCHAR[asset_path.size() + 1];
-    DWORD directory_watch_wait_status;
     strcpy(lp_asset_path, asset_path.c_str());
 
     while (true) {
-        directory_watcher_change_handles[0] = FindFirstChangeNotification(
+        auto directory_watcher_change_handle = FindFirstChangeNotification(
             lp_asset_path, false, FILE_NOTIFY_CHANGE_LAST_WRITE);
 
-        if (directory_watcher_change_handles[0] == INVALID_HANDLE_VALUE) {
+        if (directory_watcher_change_handle == INVALID_HANDLE_VALUE) {
             exit(GetLastError());
         }
 
-        directory_watch_wait_status = WaitForMultipleObjects(
-            1, directory_watcher_change_handles, false, INFINITE);
+        auto directory_watch_wait_status =
+            WaitForSingleObject(directory_watcher_change_handle, INFINITE);
 
         if (directory_watch_wait_status == WAIT_OBJECT_0) {
             Sleep(10); // TODO : I think there is a race conidition here and the
