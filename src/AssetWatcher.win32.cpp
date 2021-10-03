14e72bd6847ddaf10103c3cd68855ac0f191ba1f
Tom Sullivan <tom@msbit.com.au>
2021-10-04 01:56:47 +1100

    Shorten variable names

--- src/AssetWatcher.win32.cpp
+++ src/AssetWatcher.win32.cpp
@@ -7,17 +7,16 @@ void AssetWatcher::StartWatching() {
     auto asset_path = std::filesystem::absolute("assets");
 
     while (!shutdown) {
-        auto directory_watcher_change_handle = FindFirstChangeNotification(
+        auto change_handle = FindFirstChangeNotification(
             asset_path.string().c_str(), false, FILE_NOTIFY_CHANGE_LAST_WRITE);
 
-        if (directory_watcher_change_handle == INVALID_HANDLE_VALUE) {
+        if (change_handle == INVALID_HANDLE_VALUE) {
             exit(GetLastError());
         }
 
-        auto directory_watch_wait_status =
-            WaitForSingleObject(directory_watcher_change_handle, 5000);
+        auto wait_status = WaitForSingleObject(change_handle, 5000);
 
-        if (directory_watch_wait_status == WAIT_OBJECT_0) {
+        if (wait_status == WAIT_OBJECT_0) {
             Sleep(10); // TODO : I think there is a race conidition here and the
                        // file is still locked
             required_reload = true;
