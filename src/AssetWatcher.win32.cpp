f3bf63f3b340be123f140442496fb00d45c97ba7
Tom Sullivan <tom@msbit.com.au>
2021-10-04 01:59:41 +1100

    Use `FindNextChangeNotification` and `FindCloseChangeNotification`
    As per:
    
      https://docs.microsoft.com/en-us/windows/win32/fileio/obtaining-directory-change-notifications
    
    create the handle using `FindFirstChangeNotification` outside the
    loop, and use `FindNextChangeNotification` on receiving an event
    for that handle. When exitting the thread, also call
    `FindCloseChangeNotification` to more gracefully cease watching.

--- src/AssetWatcher.win32.cpp
+++ src/AssetWatcher.win32.cpp
@@ -6,20 +6,26 @@
 void AssetWatcher::StartWatching() {
     auto asset_path = std::filesystem::absolute("assets");
 
+    auto change_handle = FindFirstChangeNotification(
+        asset_path.string().c_str(), false, FILE_NOTIFY_CHANGE_LAST_WRITE);
+
+    if (change_handle == INVALID_HANDLE_VALUE) {
+        exit(GetLastError());
+    }
+
     while (!shutdown) {
-        auto change_handle = FindFirstChangeNotification(
-            asset_path.string().c_str(), false, FILE_NOTIFY_CHANGE_LAST_WRITE);
-
-        if (change_handle == INVALID_HANDLE_VALUE) {
-            exit(GetLastError());
-        }
-
         auto wait_status = WaitForSingleObject(change_handle, 5000);
 
         if (wait_status == WAIT_OBJECT_0) {
             Sleep(10); // TODO : I think there is a race conidition here and the
                        // file is still locked
             required_reload = true;
+
+            if (FindNextChangeNotification(change_handle) == false) {
+                exit(GetLastError());
+            }
         }
     }
+
+    FindCloseChangeNotification(change_handle);
 }
